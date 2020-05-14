c6c6de5a730ec1d940203a06e935f1d7f81d3fbe
Tarun sharma <tarunsharma908427@gmail.com>
2020-05-14 15:15:16 +0530

    for loop update

--- ForLoop.java
+++ ForLoop.java
@@ -5,6 +5,8 @@ public static void main(String[]args){
 for(int i=0;i<=4;i++){
 System.out.println("hello world");
 }
+System.out.println("hello world1");
+System.out.println("hello world2");
 
 }
 }
